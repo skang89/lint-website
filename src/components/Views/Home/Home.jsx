dee2b72ee6548bea59a17c2980fd9641fa27446c
Shiv Kang <shivkang89@gmail.com>
2020-04-27 20:16:40 +0100

    Styled desktop layout of landing page

--- src/components/Views/Home/Home.jsx
+++ src/components/Views/Home/Home.jsx
@@ -1,14 +1,32 @@
 // import react packages
 import React from "react";
+import Slider from "react-slick";
 
 // import styles
 import "./Home.scss";
 
 const Home = () => {
+  // const sliderSettings = {
+  //   autoplay: true, // is true
+  //   autoplaySpeed: 2250,
+  //   arrows: false,
+  //   className: "lc-slider",
+  //   draggable: false, // is false
+  //   fade: true,
+  //   infinite: true,
+  //   speed: 500,
+  //   pauseOnHover: false,
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   swipe: false, // is false
+  //   touchMove: false, // is false
+  // };
+
   return (
     <div className="lc-wrapper">
       <img src="/assets/img/LINT_Logo.svg" alt="Lint Cosmetics" className="lc-logo" />
       <section className="lc-slider">
+        {/* <Slider {...sliderSettings}> */}
         <div className="lc-slider__item">
           <img
             src="/assets/img/So-Blushing.jpg"
@@ -16,9 +34,10 @@ const Home = () => {
             className="lc-slider__img"
           />
         </div>
+        {/* </Slider> */}
       </section>
       <section className="lc-text">
-        <p>2020</p>
+        <p>August 2020</p>
         <p>
           Pro Artist Tools
           <br />
