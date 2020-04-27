3e53882cfd4c3e09a33fd52b6577e2cd97b37002
Shiv Kang <shivkang89@gmail.com>
2020-04-27 17:00:41 +0100

    amend mobile styles and begun styling large device layout

--- src/components/Views/Home/Home.jsx
+++ src/components/Views/Home/Home.jsx
@@ -7,13 +7,7 @@ import "./Home.scss";
 const Home = () => {
   return (
     <div className="lc-wrapper">
-      <section className="lc-logo__container">
-        <img
-          src="/assets/img/LINT_Logo.svg"
-          alt="Lint Cosmetics"
-          className="lc-logo__logo"
-        />
-      </section>
+      <img src="/assets/img/LINT_Logo.svg" alt="Lint Cosmetics" className="lc-logo" />
       <section className="lc-slider">
         <div className="lc-slider__item">
           <img
@@ -32,6 +26,27 @@ const Home = () => {
         </p>
       </section>
     </div>
+
+    // <div className="lc-wrapper lc-container ">
+    //   <img src="/assets/img/LINT_Logo.svg" alt="Lint Cosmetics" className="lc-logo " />
+    //   <div className="lc-slider ">
+    //     <div className="lc-slider__item">
+    //       <img
+    //         src="/assets/img/So-Blushing.jpg"
+    //         alt="So Blushing"
+    //         className="lc-slider__img"
+    //       />
+    //     </div>
+    //   </div>
+    //   <div className="lc-text ">
+    //     <p>2020</p>
+    //     <p>
+    //       Pro Artist Tools
+    //       <br />
+    //       Coming Soon
+    //     </p>
+    //   </div>
+    // </div>
   );
 };
 
