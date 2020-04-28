3ac41c6b10519a1bcb81c0c6ab06ad07d5fd4244
Shiv Kang <shivkang89@gmail.com>
2020-04-28 11:00:53 +0100

    added images and slide fade effect

--- src/components/Views/Home/Home.jsx
+++ src/components/Views/Home/Home.jsx
@@ -6,27 +6,27 @@ import Slider from "react-slick";
 import "./Home.scss";
 
 const Home = () => {
-  // const sliderSettings = {
-  //   autoplay: true, // is true
-  //   autoplaySpeed: 2250,
-  //   arrows: false,
-  //   className: "lc-slider",
-  //   draggable: false, // is false
-  //   fade: true,
-  //   infinite: true,
-  //   speed: 500,
-  //   pauseOnHover: false,
-  //   slidesToShow: 1,
-  //   slidesToScroll: 1,
-  //   swipe: false, // is false
-  //   touchMove: false, // is false
-  // };
+  const sliderSettings = {
+    autoplay: true, // is true
+    autoplaySpeed: 2500,
+    arrows: false,
+    centerMode: true,
+    className: "lc-slider",
+    draggable: false, // is false
+    fade: true,
+    infinite: true,
+    speed: 500,
+    pauseOnHover: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    swipe: true, // is false
+    touchMove: true, // is false
+  };
 
   return (
     <div className="lc-wrapper">
       <img src="/assets/img/LINT_Logo.svg" alt="Lint Cosmetics" className="lc-logo" />
-      <section className="lc-slider">
-        {/* <Slider {...sliderSettings}> */}
+      <Slider {...sliderSettings}>
         <div className="lc-slider__item">
           <img
             src="/assets/img/So-Blushing.jpg"
@@ -34,16 +34,34 @@ const Home = () => {
             className="lc-slider__img"
           />
         </div>
-        {/* </Slider> */}
-      </section>
-      <section className="lc-text">
+        <div className="lc-slider__item">
+          <img src="/assets/img/Danny.jpg" alt="Danny" className="lc-slider__img" />
+        </div>
+        <div className="lc-slider__item">
+          <img
+            src="/assets/img/Malowanie.jpg"
+            alt="Malowanie"
+            className="lc-slider__img"
+          />
+        </div>
+        <div className="lc-slider__item">
+          <img src="/assets/img/David.jpg" alt="David" className="lc-slider__img" />
+        </div>
+        <div className="lc-slider__item">
+          <img src="/assets/img/Hialex.jpg" alt="Hialex" className="lc-slider__img" />
+        </div>
+        <div className="lc-slider__item">
+          <img src="/assets/img/Louther.jpg" alt="Louther" className="lc-slider__img" />
+        </div>
+      </Slider>
+      <div className="lc-text">
         <p>August 2020</p>
         <p>
           Pro Artist Tools
           <br />
           Coming Soon
         </p>
-      </section>
+      </div>
     </div>
 
     // <div className="lc-wrapper lc-container ">
