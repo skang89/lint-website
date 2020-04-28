73007400dd1a919f31446f8b39dcd90cb5beb8e0
Shiv Kang <shivkang89@gmail.com>
2020-04-28 11:59:04 +0100

    update web manifest and add device icons

--- src/components/Views/Home/Home.jsx
+++ src/components/Views/Home/Home.jsx
@@ -7,20 +7,20 @@ import "./Home.scss";
 
 const Home = () => {
   const sliderSettings = {
-    autoplay: true, // is true
+    autoplay: true,
     autoplaySpeed: 2500,
     arrows: false,
     centerMode: true,
     className: "lc-slider",
-    draggable: false, // is false
+    draggable: false,
     fade: true,
     infinite: true,
     speed: 500,
     pauseOnHover: false,
     slidesToShow: 1,
     slidesToScroll: 1,
-    swipe: true, // is false
-    touchMove: true, // is false
+    swipe: true,
+    touchMove: true,
   };
 
   return (
@@ -63,27 +63,6 @@ const Home = () => {
         </p>
       </div>
     </div>
-
-    // <div className="lc-wrapper lc-container ">
-    //   <img src="/assets/img/LINT_Logo.svg" alt="Lint Cosmetics" className="lc-logo " />
-    //   <div className="lc-slider ">
-    //     <div className="lc-slider__item">
-    //       <img
-    //         src="/assets/img/So-Blushing.jpg"
-    //         alt="So Blushing"
-    //         className="lc-slider__img"
-    //       />
-    //     </div>
-    //   </div>
-    //   <div className="lc-text ">
-    //     <p>2020</p>
-    //     <p>
-    //       Pro Artist Tools
-    //       <br />
-    //       Coming Soon
-    //     </p>
-    //   </div>
-    // </div>
   );
 };
 
