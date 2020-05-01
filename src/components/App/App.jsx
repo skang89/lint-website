9507b82a862e6c7a529533f71c0dce9313dee177
Shiv Kang <shivkang89@gmail.com>
2020-05-01 14:56:03 +0100

    update app route path for github pages

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/lint-website/" component={Home} />
       </Switch>
     </Router>
   );
