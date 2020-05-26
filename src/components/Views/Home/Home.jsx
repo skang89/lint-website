867b17986a4da16ac1a7d58e9e038226ce03a3dd
Shiv Kang <shivkang89@gmail.com>
2020-05-26 20:16:38 +0100

    set up sub header style classes boby type classes

--- src/components/Views/Home/Home.jsx
+++ src/components/Views/Home/Home.jsx
@@ -50,6 +50,104 @@ const Home = () => {
         This is Lint Cosmetics Subheader 4 - Bold
       </h1>
       <h1 className="lc-type__subheader__h4">This is Lint Cosmetics Subheader 4</h1>
+      <h1 className="lc-type__subheader__h5 lc-type__subheader__h5--bold lc-type__subheader__h5--light">
+        This is Lint Cosmetics Subheader 5 - Light
+      </h1>
+      <h1 className="lc-type__subheader__h5 lc-type__subheader__h5--bold">
+        This is Lint Cosmetics Subheader 5 - Bold
+      </h1>
+      <h1 className="lc-type__subheader__h5">This is Lint Cosmetics Subheader 5</h1>
+      <h1 className="lc-type__subheader__h6 lc-type__subheader__h6--bold lc-type__subheader__h6--light">
+        This is Lint Cosmetics Subheader 6 - Light
+      </h1>
+      <h1 className="lc-type__subheader__h6 lc-type__subheader__h6--bold">
+        This is Lint Cosmetics Subheader 6 - Bold
+      </h1>
+      <h1 className="lc-type__subheader__h6">This is Lint Cosmetics Subheader 6</h1>
+      <h1 className="lc-type__subheader__h7 lc-type__subheader__h7--bold lc-type__subheader__h7--light">
+        This is Lint Cosmetics Subheader 7 - Light
+      </h1>
+      <h1 className="lc-type__subheader__h7 lc-type__subheader__h7--bold">
+        This is Lint Cosmetics Subheader 7 - Bold
+      </h1>
+      <h1 className="lc-type__subheader__h7">This is Lint Cosmetics Subheader 7</h1>
+      <br />
+      <br />
+      <p className="lc-type__body__b1">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci harum, sed nam
+        quam vero aspernatur placeat ut eius possimus ipsa nostrum pariatur reiciendis
+        accusantium porro quae perferendis quis similique? Quasi.
+      </p>
+      <p className="lc-type__body__b1--bold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci harum, sed nam
+        quam vero aspernatur placeat ut eius possimus ipsa nostrum pariatur reiciendis
+        accusantium porro quae perferendis quis similique? Quasi.
+      </p>
+      <p className="lc-type__body__b1--light">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci harum, sed nam
+        quam vero aspernatur placeat ut eius possimus ipsa nostrum pariatur reiciendis
+        accusantium porro quae perferendis quis similique? Quasi.
+      </p>
+      <p className="lc-type__body__b2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci harum, sed nam
+        quam vero aspernatur placeat ut eius possimus ipsa nostrum pariatur reiciendis
+        accusantium porro quae perferendis quis similique? Quasi.
+      </p>
+      <p className="lc-type__body__b2 lc-type__body__b2--bold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci harum, sed nam
+        quam vero aspernatur placeat ut eius possimus ipsa nostrum pariatur reiciendis
+        accusantium porro quae perferendis quis similique? Quasi.
+      </p>
+      <p className="lc-type__body__b2--light">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci harum, sed nam
+        quam vero aspernatur placeat ut eius possimus ipsa nostrum pariatur reiciendis
+        accusantium porro quae perferendis quis similique? Quasi.
+      </p>
+      <p className="lc-type__body__b3">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci harum, sed nam
+        quam vero aspernatur placeat ut eius possimus ipsa nostrum pariatur reiciendis
+        accusantium porro quae perferendis quis similique? Quasi.
+      </p>
+      <p className="lc-type__body__b3--bold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci harum, sed nam
+        quam vero aspernatur placeat ut eius possimus ipsa nostrum pariatur reiciendis
+        accusantium porro quae perferendis quis similique? Quasi.
+      </p>
+      <p className="lc-type__body__b3--light">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci harum, sed nam
+        quam vero aspernatur placeat ut eius possimus ipsa nostrum pariatur reiciendis
+        accusantium porro quae perferendis quis similique? Quasi.
+      </p>
+      <p className="lc-type__body__b4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci harum, sed nam
+        quam vero aspernatur placeat ut eius possimus ipsa nostrum pariatur reiciendis
+        accusantium porro quae perferendis quis similique? Quasi.
+      </p>
+      <p className="lc-type__body__b4--bold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci harum, sed nam
+        quam vero aspernatur placeat ut eius possimus ipsa nostrum pariatur reiciendis
+        accusantium porro quae perferendis quis similique? Quasi.
+      </p>
+      <p className="lc-type__body__b4--light">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci harum, sed nam
+        quam vero aspernatur placeat ut eius possimus ipsa nostrum pariatur reiciendis
+        accusantium porro quae perferendis quis similique? Quasi.
+      </p>
+      <p className="lc-type__body__b5">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci harum, sed nam
+        quam vero aspernatur placeat ut eius possimus ipsa nostrum pariatur reiciendis
+        accusantium porro quae perferendis quis similique? Quasi.
+      </p>
+      <p className="lc-type__body__b5--bold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci harum, sed nam
+        quam vero aspernatur placeat ut eius possimus ipsa nostrum pariatur reiciendis
+        accusantium porro quae perferendis quis similique? Quasi.
+      </p>
+      <p className="lc-type__body__b5--light">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci harum, sed nam
+        quam vero aspernatur placeat ut eius possimus ipsa nostrum pariatur reiciendis
+        accusantium porro quae perferendis quis similique? Quasi.
+      </p>
     </>
   );
 };
