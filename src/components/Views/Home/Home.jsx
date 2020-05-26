c745d78a1ac0b5a5371462833d9c314ce51964ea
Shiv Kang <shivkang89@gmail.com>
2020-05-26 17:13:32 +0100

    added font mixin and working on type classes

--- src/components/Views/Home/Home.jsx
+++ src/components/Views/Home/Home.jsx
@@ -8,7 +8,48 @@ const Home = () => {
   return (
     <>
       <Header />
-      <h1>This is Lint Cosmetics</h1>
+      <h1 className="lc-type__header__h1">This is Lint Cosmetics Header 1</h1>
+      <h1 className="lc-type__header__h1 lc-type__header__h1--light">
+        This is Lint Cosmetics Header 1 - Light
+      </h1>
+      <h1 className="lc-type__header__h2">This is Lint Cosmetics Header 2</h1>
+      <h1 className="lc-type__header__h2 lc-type__header__h2--light">
+        This is Lint Cosmetics Header 2
+      </h1>
+      <h1 className="lc-type__header__h3">This is Lint Cosmetics Header 3</h1>
+      <h1 className="lc-type__header__h3 lc-type__header__h3--light">
+        This is Lint Cosmetics Header 3 - Light
+      </h1>
+      <br />
+      <br />
+      <h1 className="lc-type__subheader__h1 lc-type__subheader__h1--bold lc-type__subheader__h1--light">
+        This is Lint Cosmetics Subheader 1 - Light
+      </h1>
+      <h1 className="lc-type__subheader__h1 lc-type__subheader__h1--bold">
+        This is Lint Cosmetics Subheader 1 - Bold
+      </h1>
+      <h1 className="lc-type__subheader__h1">This is Lint Cosmetics Subheader 1</h1>
+      <h1 className="lc-type__subheader__h2 lc-type__subheader__h2--bold lc-type__subheader__h2--light">
+        This is Lint Cosmetics Subheader 2 - Light
+      </h1>
+      <h1 className="lc-type__subheader__h2 lc-type__subheader__h2--bold">
+        This is Lint Cosmetics Subheader 2 - Bold
+      </h1>
+      <h1 className="lc-type__subheader__h2">This is Lint Cosmetics Subheader 2</h1>
+      <h1 className="lc-type__subheader__h3 lc-type__subheader__h3--bold lc-type__subheader__h3--light">
+        This is Lint Cosmetics Subheader 3 - Light
+      </h1>
+      <h1 className="lc-type__subheader__h3 lc-type__subheader__h3--bold">
+        This is Lint Cosmetics Subheader 3 - Bold
+      </h1>
+      <h1 className="lc-type__subheader__h3">This is Lint Cosmetics Subheader 3</h1>
+      <h1 className="lc-type__subheader__h4 lc-type__subheader__h4--bold lc-type__subheader__h4--light">
+        This is Lint Cosmetics Subheader 4 - Light
+      </h1>
+      <h1 className="lc-type__subheader__h4 lc-type__subheader__h4--bold">
+        This is Lint Cosmetics Subheader 4 - Bold
+      </h1>
+      <h1 className="lc-type__subheader__h4">This is Lint Cosmetics Subheader 4</h1>
     </>
   );
 };
