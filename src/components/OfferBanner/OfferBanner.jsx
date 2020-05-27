16803b1d622b7014a0f1f2b23e6443725691714c
Shiv Kang <shivkang89@gmail.com>
2020-05-28 00:42:41 +0100

    styled offer banner

---
+++ src/components/OfferBanner/OfferBanner.jsx
@@ -0,0 +1,21 @@
+// import react packages
+import React from "react";
+import { Link } from "react-router-dom";
+
+const OfferBanner = () => {
+  return (
+    <div className="bx--grid bx--grid--full-width lc-offer-banner">
+      <div className="bx--row">
+        <div className="bx--col">
+          <Link to="/" title="Banner title">
+            <h3 className="lc-type__meta__s1 lc-type__meta__s1--light lc-type__meta__s1--bold lc-text__center lc-offer-banner__headline">
+              This is some text for an offer <span></span>
+            </h3>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OfferBanner;
--- src/components/Views/Home/Home.jsx
+++ src/components/Views/Home/Home.jsx
@@ -3,11 +3,13 @@ import React from "react";
 
 // import components
 import Header from "../../Header/Header";
+import OfferBanner from "../../OfferBanner/OfferBanner";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <OfferBanner />
       <h1 className="lc-type__header__h1">This is Lint Cosmetics Header 1</h1>
     </>
   );
