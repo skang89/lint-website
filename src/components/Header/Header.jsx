62c2a6beb7c8a89862d6576ad927de83afb82629
Shiv Kang <shivkang89@gmail.com>
2020-05-27 23:21:20 +0100

    styled header, but not nav menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,30 +5,33 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div className="bx--grid bx--grid--full-width lc-header">
-      <div className="bx--row">
-        <div className="bx--col bx--col-md-6 bx--col-lg-12 bx--col-xlg-14 bx--col-max-14 lc-header__logo">
+      <div className="bx--row lc-align__items--center">
+        <div className="bx--col bx--col-md-6 bx--col-lg-13 bx--col-xlg-13 bx--col-max-14 lc-header__logo">
           <div>
-            <Link to="/">
+            <Link to="/" title="Lint Cosmetics">
               <img src="/assets/logos/LINT_Logo.svg" alt="Lint Cosmetics" />
             </Link>
           </div>
         </div>
-        <div className="bx--col bx--col-md-2 bx--col-lg-4 bx--col-xlg-2 bx--col-max-2">
-          <div className="bx--row">
-            <div className="bx--col ">
+        <div className="bx--col bx--col-md-2 bx--col-lg-3 bx--col-xlg-3 bx--col-max-2 lc-header__items">
+          <div className="bx--row lc-align__items--center">
+            <div className="bx--col lc-header__items__currency">
               <span className="lc-type__header__h1">£</span>
             </div>
-            <div className="bx--col">
-              <img
-                src="assets/icons/icons-header-shopping-bag-small.svg"
-                alt="shopping basket"
-              />
+            <div className="bx--col lc-header__items__basket">
+              <Link to="/" title="Basket items: 0">
+                <img
+                  src="assets/icons/icons-header-shopping-bag-small.svg"
+                  alt="shopping basket"
+                />
+                <span className="lc-header__items__basket__number">0</span>
+              </Link>
             </div>
-            <div className="bx--col">
-              <button type="button">
-                <span>-</span>
-                <span>-</span>
-                <span>-</span>
+            <div className="bx--col lc-header__items__menu">
+              <button type="button" className="lc-button lc-button__hamburger">
+                <span className="lc-button__hamburger__line lc-button__hamburger__line--top"></span>
+                <span className="lc-button__hamburger__line lc-button__hamburger__line--middle"></span>
+                <span className="lc-button__hamburger__line lc-button__hamburger__line--bottom"></span>
               </button>
             </div>
           </div>
